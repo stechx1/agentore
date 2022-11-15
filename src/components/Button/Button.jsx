ca9e5ec2afd85018614e364968a77135f7a6bd8e
techx1 <ss.uitech01@gmail.com>
2022-11-15 23:46:03 +0500

    added welcome page

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-export const Button = ({ type, onClick, children }) => {
+export const Button = ({ paddingX, type, onClick, children }) => {
   if (type === 'secondary') {
     return (
-      <div onClick={onClick} className='text-primary border border-primary rounded-full px-5 py-2 flex space-x-2 hover:cursor-pointer hover:bg-primary hover:text-black'>
+      <div
+        onClick={onClick}
+        className='text-primary border border-primary rounded-full px-5 py-2 flex space-x-2 hover:cursor-pointer hover:bg-primary hover:text-black'
+      >
         {children}
       </div>
     );
   } else {
     return (
-      <div onClick={onClick} className='bg-primary text-black px-5 py-2 rounded-full flex space-x-14 items-center hover:cursor-pointer hover:bg-primary '>
+      <div
+        onClick={onClick}
+        className={`bg-primary text-black px-5 py-2 rounded-full flex ${
+          paddingX ? `space-x-${paddingX}` : 'space-x-14'
+        }  items-center hover:cursor-pointer hover:bg-primary`}
+      >
         <button>{children}</button>
         <img width={20} height={20} src='/icons/arrow-icon.png' alt='arrow' />
       </div>
---
+++ src/pages/welcome/index.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { Button } from '../../components/Button/Button';
+
+const WelcomePage = () => {
+  return (
+    <div className='h-screen bg-[#212121] flex justify-center items-center'>
+      <Head>
+        <title>Welcome - Agentore</title>
+      </Head>
+      <div className='bg-[#111111] text-white rounded-xl px-32 py-20 relative'>
+        <img
+          src='/icons/man-icon.png'
+          alt='man-icon'
+          className='absolute top-[-30px] left-7 '
+        />
+        <h2 className='text-3xl mb-8'>Welcome to Agentore</h2>
+
+        <div>
+          <p className='max-w-[400px]'>
+            My name is Agentron and I am the first agent of Agentore. I have
+            been designed to help you training your agent to manage your
+            customer inquiries and work smarter.
+          </p>
+        </div>
+        <div className='flex space-x-2 mt-6'>
+          <Button paddingX="36">Agent Registration Form</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
