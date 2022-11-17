8acb0354a56a2990fd2ae12bc798b1a6bc088481
techx1 <ss.uitech01@gmail.com>
2022-11-17 13:45:28 +0500

    edited add button

--- src/components/Button/ActionButton/ActionButton.jsx
+++ src/components/Button/ActionButton/ActionButton.jsx
@@ -3,7 +3,7 @@ export const ActionButton = ({ children, color, iconSrc }) => {
   return (
     <div
       style={{ background: color }}
-      className='flex justify-between text-black px-5 py-2 rounded-full items-center w-full space-x-6'
+      className='flex justify-between text-black px-5 py-2 rounded-full items-center w-full space-x-6 hover:cursor-pointer'
     >
       <p>{children}</p>
       <img width={20} height={20} src={iconSrc} alt='arrow icon' />
--- src/components/Button/AddButton/AddButton.jsx
+++ src/components/Button/AddButton/AddButton.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-export const AddButton = ({ revert }) => {
+export const AddButton = ({ revert, onClick }) => {
   return (
-    <div
+    <div onClick={onClick}
       className={`${
         revert ? 'bg-[#D9D9D9] text-black' : 'bg-black text-white'
       } font-inter flex items-center px-4 pr-20 py-1 rounded-xl space-x-2`}
